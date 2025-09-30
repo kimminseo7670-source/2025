5b84da6db65ed3e1b74dc798f80a04749da559e3
kimminseo7670-source <kimminseo7670@duksung.ac.kr>
2025-09-30 18:01:04 +0900

    Add files via upload

---
+++ 2025 python/label2.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+root = Tk()
+
+Label(root, text='Times 폰트와 짤강색을 사용한다.',fg='red',font='Times 32 bold italic').pack()
+
+Label(root, text='Helvetica 폰트와 파란색을 사용한다.',fg='blue',bg='yellow',font='Helvetica 32 bold italic').pack()
+
+root.mainloop()
+
